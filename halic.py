4d2717a843a7856be14da8f08ea92fe3a90304f0
bnymnclskn01 <83185397+bnymnclskn01@users.noreply.github.com>
2021-10-30 13:45:32 +0300

    Ders Örnekleri

---
+++ halic.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 11:04:01 2021
+
+@author: PC
+"""
+"""sayi1=float(input("1.nci sayıyı giriniz : "))
+sayi2=float(input("2.ci sayıyı giriniz :"))
+sayi1+=sayi2
+print(sayi1)"""
+#"""public class Calisan(){}""""
+"""
+class Calisan():
+    def __init__(self,isim):
+        self.isim=isim
+        self.kabiliyetleri=[]
+        self.personel_Ekle()
+    def __personel_ekle__(self):
+        self.personel.append(self.isim)
+        print('{} adlı kişi personele eklendi' ,self.isim)
+    def personel_Goruntule(self):
+        print('Personel Listesi')
+        for kisi in self.personel:
+            print(kisi)
+    def kabiliyet_ekle(self, kabiliyet):
+        self.kabiliyetleri.append(kabiliyet)
+    def kabiliyetleri_goruntule(self):
+        print('{} adlı kişi personel kabiliyetleri' ,self.isim)
+        for kabiliyet in self.kabiliyetleri:
+            print(kabiliyet)"""
+            
+"""v=float(input("vize notunuzu gieiniz:"))
+if v>100:
+    print("100den küçük ayı girin.")
+f=float(input("final notunu gririniz:")) 
+if f>100:
+    print("100den küçük ayı girin.")   
+not1=((v*40)/100)+((f*60)/100)
+if not1>50:
+    print("Tebrikler geçtiniz.")
+elif not1<50:
+    print("kaldını<")
+    b=float(input("kaldınız büt notunuzu giriniz"))
+    if b>101:
+        print("100dn büyük not olmaz.")
+    not2=((v*40)/100)+((b*60)/100)
+    if not2>50:
+                print("geçtiniz.")
+    elif not2<50:
+                print("seneye göüşürüz..")"""
+                
+"""
+1:) Sinema otomasyon sistemi olacak öğrenci indirimi 
+    %50 cola mısır 250TL 50TL olacak ve bilet satılacak.
+2:) Uçak otomasyonu uçağımız Yunanistan 750TL 30kg bagaj  kg başına 200TL 
+3:) Öğretmenlerin Kıdemleri Olacak 5 proje çalışması olan kişi öğr grv. 
+    8 makale veya proje çalışması Dr. Öğr grv. 15 proje ve makale çalışması olan Doç Dr. Öğr. Grv olacak
+""""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+           
+            
+            
+            
+            
+            
+            
+            
+            
+     
+       
+            
+            
+            
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
